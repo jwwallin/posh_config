45934dcb052cd8f692a80c80b06ede0b9a946f05
Wallin <jussi.wallin@cgi.com>
2019-03-13 10:16:04 +0200

    Added git shortcuts and set VS Code as default.

---
+++ development.ps1
@@ -0,0 +1,24 @@
+
+# vscode
+Add-PathVariable "${env:LOCALAPPDATA}\Programs\Microsoft VS Code\bin" 
+
+# For git rebasing
+# --wait required, see https://github.com/Microsoft/vscode/issues/23219 
+$env:EDITOR = 'code --wait'
+
+# Kinda like $EDITOR in nix
+function edit {
+	& "code" -g @args
+}
+
+function gs {
+  & git status @args
+}
+
+function ga {
+  & git add @args
+}
+
+function gcom {
+  & git commit @args
+}
