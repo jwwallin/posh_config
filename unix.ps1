890c6712714f3ee58f9d5bcf5c698a93737e6190
Wallin <jussi.wallin@cgi.com>
2019-03-13 15:24:34 +0200

    fix new-line stuff.

--- unix.ps1
+++ unix.ps1
@@ -6,6 +6,7 @@ Set-PSReadlineKeyHandler -Chord Tab -Function MenuComplete
 # Like Unix touch, creates new files and updates time on old ones
 # PSCX has a touch, but it doesn't make empty files
 Remove-Alias -Name touch
+
 function touch($file) {
 	if ( Test-Path $file ) {
 		Set-FileTime $file
